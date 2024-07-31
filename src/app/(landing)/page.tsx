3c49652faf04d7c4e6665b730536291eb6dc682a
pratikgrv <pratikgaurav37@gmail.com>
2024-07-31 17:41:47 +0530

    ui :art:: path

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,35 +1,63 @@
-import React from 'react'
-import Image from 'next/image';
-import { Button } from 'src/components/ui/button';
+import React from "react";
+import Image from "next/image";
+import { Button } from "src/components/ui/button";
+import { BookDashed, Box, Inbox } from "lucide-react";
+import Sidebar from "src/components/sidebar/Sidebar";
+
+
+const ROOT_PATH = "/";
+
+export const sidebarItems = [
+	{
+		href: "dashboard",
+		label: "Dashboard",
+		icon: <BookDashed />,
+	},
+	{
+		href: "#",
+		label: "Kanban",
+		icon: <Box />,
+		// badgeText: "Pro",
+		// badgeColor: "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300",
+	},
+	{
+		href: "#",
+		label: "Inbox",
+		icon: <Inbox />,
+	},
+	// Add more items as needed
+];
 const LandingPage = () => {
-  return (
+	return (
 		<>
-			<nav className="flex justify-between p-2">
-				<Image
-					src="images/icon.svg"
-					height={40}
-					width={40}
-					className=""
-					alt="Directory"
-				/>
-				<Button>submit request</Button>
-			</nav>
-			<div className="p-4  grid grid-cols-1  md:grid-cols-2 gap-4">
-				<h1 className="md:p-12 font-heading text-5xl items-center flex justify-center ">
-					Discover new saas for your business
-				</h1>
-				<div>
+			<Sidebar items={sidebarItems} currentPath={ROOT_PATH}>
+				<nav className="flex justify-between p-2">
 					<Image
 						src="images/icon.svg"
-						height={320}
-						width={400}
+						height={40}
+						width={40}
 						className=""
 						alt="Directory"
 					/>
+					<Button>submit request</Button>
+				</nav>
+				<div className="p-4  grid grid-cols-1  md:grid-cols-2 gap-4">
+					<h1 className="md:p-12 font-heading text-5xl items-center flex justify-center ">
+						Discover new saas for your business
+					</h1>
+					<div>
+						<Image
+							src="images/icon.svg"
+							height={320}
+							width={400}
+							className=""
+							alt="Directory"
+						/>
+					</div>
 				</div>
-			</div>
+			</Sidebar>
 		</>
 	);
-}
+};
 
-export default LandingPage
+export default LandingPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "src/styles/globals.css";
@@ -6,42 +7,17 @@ import { ThemeProvider } from "src/providers/ThemeProvider";
 import Sidebar from "src/components/sidebar/Sidebar";
 import { boldSans } from "src/fonts/font";
 import { cn } from "src/lib/utils";
-import { BookDashed, Box, Inbox } from "lucide-react";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
-
+  
 export const metadata: Metadata = {
 	title: "Directory",
 	description: "all in one place for your apps",
 };
 
-const sidebarItems = [
-	{
-		href: "#",
-		label: "Dashboard",
-		icon: (
-			<BookDashed/>
-		),
-	},
-	{
-		href: "#",
-		label: "Kanban",
-		icon: (
-			<Box/>
-		),
-		badgeText: "Pro",
-		badgeColor: "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300",
-	},
-	{
-		href: "#",
-		label: "Inbox",
-		icon: (
-			<Inbox/>
-		),
-	
-	},
-	// Add more items as needed
-];
+
 
 export default function RootLayout({
 	children,
@@ -63,7 +39,7 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					<Sidebar items={sidebarItems}>{children}</Sidebar>
+					{children}
 				</ThemeProvider>
 			</body>
 		</html>
